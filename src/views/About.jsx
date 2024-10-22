b8a487c9bcb2484a0d90e80a80dd55ef97fd0826
johwolf <johannes.wolf98@gmail.com>
2024-10-22 16:57:36 +0200

    center

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -20,7 +20,7 @@ export const About = () => {
           }}
         >
           <h1 className="mb-5 text-2xl font-bold uppercase">Meine Vision</h1>
-          <p> Mein Ziel ist es, Unternehmen mit modernen Webseiten zu einem überzeugenden Auftritt und langfristiger Kundenzufriedenheit zu verhelfen. Ich biete maßgeschneiderte Lösungen zu attraktiven Konditionen und habe mich auf KMUs im B2C-Bereich, wie Restaurants, Handwerksbetriebe und Einzelhändler, spezialisiert. </p>
+          <p> Mein Ziel ist es, Unternehmen mit modernen Webseiten zu einem überzeugenden Auftritt und langfristiger Kundenzufriedenheit zu verhelfen. Ich biete maßgeschneiderte Lösungen zu attraktiven Konditionen und habe mich auf KMU im B2C-Bereich, wie Restaurants, Handwerksbetriebe und Einzelhändler, spezialisiert. </p>
         </motion.div>
         <motion.div
           className="col-span-2 sm:justify-self-center lg:col-span-1"
@@ -31,51 +31,42 @@ export const About = () => {
             once: true,
           }}
         >
-          <div className="flex flex-col gap-10 sm:flex-row sm:gap-32">
-            <ul className="flex flex-col gap-5">
-              <li>
-                <span className="mb-2 font-bold uppercase">Name</span>
-                <p>Johannes Wolf</p>
-              </li>
-              <li>
-                <span className="mb-2 font-bold uppercase">Standort</span>
-                <p>Freiburg i. Br.</p>
-              </li>
-              <li>
-                <span className="mb-2 font-bold uppercase">Verfügbarkeit</span>
-                <p>täglich, 9-17 Uhr</p>
-              </li>
-            </ul>
-            {/* Rundes Bild mit Hover-Effekt */}
-            <div className="mt-10 sm:mt-0 relative group">
-              {/* Link, der den Hover-Effekt aktiviert */}
-              <a href="https://de.linkedin.com/in/johannes-wolf-3b5702203" className="block">
-                {/* Bild */}
-                <img
-                  src="img/profile.jpg"  // Pfad zu deinem Bild
-                  alt="Profilbild"
-                  className="h-48 w-48 rounded-full object-cover shadow-lg transition-transform duration-300 group-hover:scale-105"
-                />
+          <div className="flex flex-col items-center gap-10 sm:flex-row sm:items-start sm:gap-32">
+    <ul className="flex flex-col gap-5 text-center sm:text-start sm:items-start">
+      <li>
+        <span className="mb-2 font-bold uppercase">Name</span>
+        <p>Johannes Wolf</p>
+      </li>
+      <li>
+        <span className="mb-2 font-bold uppercase">Standort</span>
+        <p>Freiburg i. Br.</p>
+      </li>
+      <li>
+        <span className="mb-2 font-bold uppercase">Verfügbarkeit</span>
+        <p>täglich, 9-17 Uhr</p>
+      </li>
+    </ul>
+    {/* Rundes Bild mit Hover-Effekt */}
+    <div className="mt-10 sm:mt-0 relative group">
+      {/* Link, der den Hover-Effekt aktiviert */}
+      <a href="https://de.linkedin.com/in/johannes-wolf-3b5702203" className="block">
+        {/* Bild */}
+        <img
+          src="img/profile.jpg"  // Pfad zu deinem Bild
+          alt="Profilbild"
+          className="h-48 w-48 rounded-full object-cover shadow-lg transition-transform duration-300 group-hover:scale-105"
+        />
 
-                {/* Overlay für den Hover-Effekt */}
-                <div className="absolute top-0 left-0 h-48 w-48 rounded-full flex items-center justify-center bg-primary-500 rounded-full opacity-0 group-hover:opacity-70 transition-opacity duration-300 group-hover:scale-105">
-                  <span className="text-white text-sm text-center font-bold flex items-center justify-center"> 
-                    Zum <BsLinkedin className="ml-1" /> &nbsp;Profil
-                  </span>
-                </div>
-              </a>
-            </div>
-          </div>
-          { /*
-          <a
-            href="#"
-            className="mt-5 inline-flex items-center gap-2 rounded-full bg-primary-500 px-3 py-2 text-white shadow-[inset_0px_-4px_2px_rgb(0_0_0_/_25%)] hover:animate-zoom"
-          >
-            <BsDownload />
-            Download CV
-          </a>
-        */}
-        </motion.div>
+        {/* Overlay für den Hover-Effekt */}
+        <div className="absolute top-0 left-0 h-48 w-48 rounded-full flex items-center justify-center bg-primary-500 rounded-full opacity-0 group-hover:opacity-70 transition-opacity duration-300 group-hover:scale-105">
+          <span className="text-white text-sm text-center font-bold flex items-center justify-center"> 
+            Zum <BsLinkedin className="ml-1" /> &nbsp;Profil
+          </span>
+        </div>
+      </a>
+    </div>
+  </div>
+</motion.div>
       </div>
     </section>
   );
--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -19,7 +19,7 @@ export const Hero = () => {
                 </h1>
                 <ReactTyped
                   className="text-xl font-light uppercase leading-10"
-                  strings={["Surfen mit Stil", "Webdesign für KMUs"]}
+                  strings={["Surfen mit Stil", "Webdesign für KMU"]}
                   typeSpeed={40}
                   backSpeed={50}
                   loop
